0b53299f2bed4c8b8c02ddb00d288e9147ff6d62
bonasunu <bonaventurasunu@gmail.com>
2020-06-14 19:42:40 +0700

    add front end

---
+++ build/precache-manifest.ad1afc9e3b3757c2a6d507f4b5b54ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a3a0eba916f1d6f9c2d76cc4e181dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0724950028262fdd9fb7",
+    "url": "/static/css/main.0b300a9e.chunk.css"
+  },
+  {
+    "revision": "31fec337c1050141c471",
+    "url": "/static/js/2.1b266603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b266603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0724950028262fdd9fb7",
+    "url": "/static/js/main.5d87b345.chunk.js"
+  },
+  {
+    "revision": "6b189a97c36646ef317f",
+    "url": "/static/js/runtime-main.2c31142c.js"
+  }
+]);
