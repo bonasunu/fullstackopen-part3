0493c303d3206641f0f4d50f47056f7a2fefa6b0
bonasunu <bonaventurasunu@gmail.com>
2020-06-16 13:03:03 +0700

    new app with mongo

--- index.js
+++ index.js
@@ -1,96 +1,80 @@
+require('dotenv').config()
 const express = require('express')
 const { response } = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Book = require('./models/phonebook')
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 app.use(morgan('combined'))
 
-
-let persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": 1
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
-
 app.get('/', (req, res) => {
     res.send('<h1>Part 3 </h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Book.find({}).then(person => {
+        res.json(person)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(peep => peep.id === id)
-
-    if (person) {
-        res.json(person)
-    }
-    else {
-        res.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+    Book.findById(req.params.id)
+    .then(person => {
+        if (person) {
+            res.json(person)
+        }
+        else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(404).end()
+    Book.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => {
-    const maxId = Math.floor(Math.random() * 1000)
-
-    return maxId
-}
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
-    if (!body.name || !body.number) {
-        return res.status(404).json({
-            error: 'content missing'
-        })
-    }
-    else if (persons.find(peep => peep.name === body.name)) {
-        return res.status(404).json({
-            error: 'content missing'
-        })
+    if (body === undefined) {
+        console.log(body)
+        return res.status(404).json({ error: 'content missing'})
     }
 
-    const person = {
-        'name': body.name,
-        'number': body.number,
-        'id': generateId()
-    }
-
-    persons = persons.concat(person)
+    const person = new Book({
+        name: body.name,
+        number: body.number,
+    })
 
     console.log(person)
 
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson.toJSON())
+    })
+
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        number: body.number
+    }
+
+    Book.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
@@ -101,7 +85,26 @@ app.get('/info', (req, res) => {
     )
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (req , res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
